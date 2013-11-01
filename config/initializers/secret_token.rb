8519b9bee795dc5df002a61066de14212fcd1348
Sergio Schuler <sergio.schuler@gmail.com>
2013-11-01 15:47:35 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prioriter::Application.config.secret_key_base = '5335f83f10af45481dacc953fa185c24ebba82ab56b246600f49dd2a1c9ab1be6b5034b43c879ef69f01571d9bd5df9c1d2fb60e545fb5e4ffd81667777ff42a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prioriter::Application.config.session_store :cookie_store, key: '_prioriter_session'
