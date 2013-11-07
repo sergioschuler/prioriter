6495963b952bb33618b32dee76127dc8a38a5f82
Sergio Schuler <sergio.schuler@gmail.com>
2013-11-07 14:33:44 -0200

    new task is added to the bottom of the task list

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ActiveRecord::Base
 	belongs_to :user
+	before_create :set_position
+
+	private
+  def set_position
+    self.position ||= 1 + (Task.where(user_id: self.user_id, completed: false).maximum(:position) || 0)
+  end
 end
